30a79320eaf3637dfe95a9b8372c47f0cfc58c66
abhishekfasate <abhishekfasate@gmail.com>
2023-01-02 19:17:53 +0530

    Addeed URL Database

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,35 @@
-const savepassword=document.querySelector('.btn')
+const savepassword=document.getElementById('submitbtn')
+const urlbolck=document.getElementById('url')
 
-savepassword.addEventListener('click',()=>
+//Getting the URL name to make sure to save the password for particular site
+chrome.tabs.query(
+    {'active': true, 
+    'lastFocusedWindow': true
+    },
+    function(tabs) 
+    {
+    var mainUrl = tabs[0].url;
+    var expression = "http[s]?:\/\/www.[a-z]*.[com|in]*"    
+    const regex = new RegExp(expression)
+    url = mainUrl.match(expression)[0]
+    urlbolck.innerHTML=`<a href="${mainUrl}">${url}</a>`
+})
+
+window.onload = (event) => {
+savepassword.addEventListener('click',(e)=>
 {   
+    e.preventDefault()
+    console.log("INSIDE JS FUNCTION",savepassword)
+    
     //Getting value of text Field Of UserName
     const username=document.getElementById('email').value;
     
     //Getting value of text Field Of Password
     const password=document.getElementById('password').value;
     
+    //Getting value of Url from the innertext field of the URL
+    const url =document.getElementById('url').innerText
+
     //open takes arguments as (databasename,version[optional])
     let indexdb=indexedDB.open('PasswordSaver',1) 
     
@@ -23,19 +45,9 @@ savepassword.addEventListener('click',()=>
         let store=tx.objectStore('data')
         store.put({
             username:username,
-            password:password
+            password:password,
+            url:url
         })
     }   
 })
-
-// function addtodb(e)
-// {
-//     e.preventDefault();
-//     const username=document.getElementById('email').value;
-//     const password=document.getElementById('password').value;
-//     alert(username,password)
-//     const request=window.indexedDB.open('data',1)
-//     request.onupgradeneeded = ()=>{
-//         request.result.createObjectStore('')
-//     };
-// }
+}
